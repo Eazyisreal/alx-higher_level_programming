b5fa2914ca7250bb87f1c42c2171405b9b55aabf
Eazyisreal <eazyisreal192@gmail.com>
2023-05-12 08:48:18 +0100

    Task done

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
-    i = 1
-    j = 2
-    print("{:d} + {:d} = {:d}".format(i, j, add(i, j)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
