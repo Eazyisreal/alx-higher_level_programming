19fefdd0f4993d4cb6dd07358e1190ff02cc6ce8
Eazyisreal <eazyisreal192@gmail.com>
2023-05-12 10:16:22 +0100

    Task done

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+
+    i = argv[2]
+    j = {"+": add, "-": sub, "*": mul, "/": div}
+    if i not in j:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, i, b, j[i](a, b)))
