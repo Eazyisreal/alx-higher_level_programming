00af8cbccbbff9160aeb7030a642a961c4dc9253
eazyisreal <eazyisreal192@gmail.com>
2023-06-06 03:28:31 -0700

    Task done

--- 0x08-python-more_classes/newline.sh
+++ 0x08-python-more_classes/newline.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-directory="/alx-higher_level_programming/0x08-more_classes"  # Replace with your target directory
+directory="/alx-higher_level_programming/0x08-python-more_classes"  # Replace with your target directory
 
 for file in "$directory"/*; do
     if [ -f "$file" ]; then
         echo "" >> "$file"
     fi
 done
+
 
