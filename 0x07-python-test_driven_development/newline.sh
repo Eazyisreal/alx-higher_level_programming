5ee7dd5473b276e2bbf4dbbba2452262f796797a
eazyisreal <eazyisreal192@gmail.com>
2023-06-07 05:49:03 -0700

    Task done

--- 0x07-python-test_driven_development/newline.sh
+++ 0x07-python-test_driven_development/newline.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-directory="/alx-higher_level_programming/0x07-python-test_driven_development  # Replace with your target directory
+directory="/alx-higher_level_programming/0x07-python-test_driven_development"  # Replace with your target directory
 
 for file in "$directory"/*; do
     if [ -f "$file" ]; then
         echo "" >> "$file"
     fi
 done
+
+
+
