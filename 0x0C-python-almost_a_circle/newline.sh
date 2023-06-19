f2d522c66f447928ab1c45b1a5e3a69f807b22ac
Eazyisreal <eazyisreal192@gmail.com>
2023-06-19 21:34:20 +0100

    Task done

---
+++ 0x0C-python-almost_a_circle/newline.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+directory="/alx-higher_level_programming/0x0C-python-almost_a_circle"  # Replace with your target directory
+
+for file in "$directory"/*; do
+    if [ -f "$file" ]; then
+        echo "" >> "$file"
+    fi
+done
+
+
+
+
