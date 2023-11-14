00b01d8ef177dfeeebbf86fee91b24e66bd44f37
Eazyisreal <eazyisreal192@gmail.com>
2023-11-14 08:57:24 +0100

    [Docs]: Updated Readme

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,22 @@
-
 #!/usr/bin/node
-function factorial (n) {
-  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
-}
-
-console.log(factorial(Number(process.argv[2])));
+const myObject = {
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  
+  /*
+  adding line below prints as a result:
+  { type: 'object', value: 12 }
+  { type: 'object', value: 13, incr: [Function] }
+  { type: 'object', value: 14, incr: [Function] }
+  { type: 'object', value: 15, incr: [Function] }
+  */
+  myObject.incr = function () { this.value += 1; };
+  
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
+  myObject.incr();
+  console.log(myObject);
